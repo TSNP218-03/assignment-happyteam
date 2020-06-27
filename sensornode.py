30480efe85759eba8b6a74ffb9209b7d140cc849
kin liang <kinliang9703@hotmail.com>
2020-06-27 22:27:56 +0800

    Proposed codebase-#2 with Python + Socket.IO

---
+++ sensornode.py
@@ -0,0 +1,21 @@
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+
+sio.disconnect()
+sio.connect('http://localhost:55555')
+#sio.wait()
